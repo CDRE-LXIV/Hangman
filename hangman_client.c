a65f0e1d696da8dbde580ca710235891cca95924
CDRE-LXIV <32503063+CDRE-LXIV@users.noreply.github.com>
2017-10-04 15:21:35 +1000

    Testing

--- hangman_client.c
+++ hangman_client.c
@@ -1,15 +1,9 @@
-#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <netdb.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include <sys/wait.h>
 #include <unistd.h>
-
-// testing edit with atom
-
-
-//testing without messing with GitHub to see what happens
