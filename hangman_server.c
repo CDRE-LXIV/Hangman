d5bc377fc6dadd0a7009fe7b437735ba22e82a91
CDRE-LXIV <32503063+CDRE-LXIV@users.noreply.github.com>
2017-10-04 12:59:23 +1000

    The hang man
    The hang man

---
+++ hangman_server.c
@@ -0,0 +1,12 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+// testing edit with atom
